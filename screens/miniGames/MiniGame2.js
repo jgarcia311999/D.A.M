b741022130d20e6ccad8c0462a5d99d135a40ba6
Jesus Garcia <jesus.garcia@iukanet.com>
2025-06-30 17:03:25 +0200

    frases hot take creadas

--- screens/miniGames/MiniGame2.js
+++ screens/miniGames/MiniGame2.js
@@ -2,11 +2,33 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableWithoutFeedback, SafeAreaView } from 'react-native';
 
 const frases = [
-  'Nunca he...',
-  'Bebe quien haya mentido hoy',
-  'Quien se haya enamorado este año, bebe',
-  'Bebe el más fiestero del grupo',
-  'Quien tenga el móvil con menos batería, bebe',
+  'No estaria con una gorda',
+  'No estaria con un calvo',
+  'No estaria con alguien sin un minimo de estudios',
+  'No estaria con alguien que fume',
+  "No estaría con alguien que se depile las cejas más que yo",
+  "No estaría con alguien que no folle bien",
+  "No estaría con alguien que no me lo coma bien",
+  "No estaría con alguien que no chupe en el primer polvo",
+  "No estaría con alguien que no beba",
+  "No estaría con alguien que diga bro cada dos palabras, bro",
+  "F*llar con calcetines debería ser delito.",
+  "Si no me haces reír, ni te me acerques.",
+  "Si no tiene habilidades sociales, fuera.",
+  "Si no me vas a sudar la camiseta, ni me la quites.",
+  "Decir ‘soy intenso’ no te hace especial, te hace pesado.",
+  "El que dice ‘no estoy buscando nada’ está buscando sexo.",
+  "Los polvos improvisados siempre ganan a los planeados.",
+  "Un beso con pique vale más que mil mensajes.",
+  "Las primeras citas deberían ser con test psicológico incluido.",
+  "Si no sabe lo que quiere, que no moleste.",
+  "El horóscopo es excusa para no madurar.",
+  "Decir ‘soy un cabrón’ no te hace interesante.",
+  "Prefiero sinceridad cruel que ghosting sutil.",
+  "Decir que ‘no crees en etiquetas’ es la nueva forma de no comprometerse.",
+  "El ‘rapidito’ no siempre es sexy, a veces es decepcionante.",
+  "Tu ex no era tóxico, tú eras adicto al drama.",
+  "No eres directa, eres gilipollas.",
 ];
 
 export default function MiniGame2() {
@@ -44,4 +66,3 @@ const styles = StyleSheet.create({
     color: '#fff'
   },
 });
-
