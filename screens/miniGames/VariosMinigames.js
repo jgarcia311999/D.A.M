58e9d1fe74db89513a8ce561e8d7c73fcfaede51
Jesus Garcia <jesus.garcia@iukanet.com>
2025-07-11 08:51:50 +0200

    cambio colores minigames

--- screens/miniGames/VariosMinigames.js
+++ screens/miniGames/VariosMinigames.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, StyleSheet, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -20,16 +20,48 @@ export default function VariosMinigames({ route }) {
       >
         <Ionicons name="arrow-back" size={28} color="#fff" />
       </TouchableOpacity>
-      <View style={{ flex: 1, backgroundColor: '#191716' }}>
+      <View style={{ flex: 1, backgroundColor: '#70B77C' }}>
         <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'flex-start', alignItems: 'center', paddingHorizontal: 24, paddingTop: insets.top + 64 }}>
           <Text style={{ fontSize: 28, fontWeight: 'bold', marginBottom: 18, color: '#fff' }}>
             {minijuego.titulo}
           </Text>
-          <Text style={{ fontSize: 20, marginBottom: 26, textAlign: 'center', color: '#d5c385' }}>
-            {minijuego.descripcionDetallada}
-          </Text>
+          <View style={styles.card}>
+            <Text style={styles.text}>
+              {minijuego.descripcionDetallada}
+            </Text>
+          </View>
         </ScrollView>
       </View>
     </>
   );
 }
+
+const { width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  card: {
+    width: width * 0.85,
+    aspectRatio: 0.625,
+    borderRadius: 20,
+    backgroundColor: '#9ED2A7',
+    borderWidth: 2,
+    borderColor: '#ffffff22',
+    shadowColor: '#fff',
+    shadowOpacity: 0.15,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 6,
+    elevation: 4,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 26,
+  },
+  text: {
+    fontSize: 20,
+    fontFamily: 'Panchang-Regular',
+    color: '#fff',
+    textAlign: 'center',
+    lineHeight: 28,
+  },
+});
