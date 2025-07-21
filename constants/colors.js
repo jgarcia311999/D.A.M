ffe3c3c163008750356a78a68ac527373eb056b6
Jesus Garcia <jesus.garcia@iukanet.com>
2025-07-21 12:43:59 +0200

    archivo colors creado

---
+++ constants/colors.js
@@ -0,0 +1,60 @@
+export default {
+    primaryText: '#000',
+    
+    backgroundHomeJugadores: '#79AFFF',
+    primaryColorHomeJugadores: '#A3C8FF',
+    secondColorHomeJugadores: '#A3C8FF',
+
+    backgroundPerfil: '#000',
+    primaryColorPerfil: '#000',
+    secondColorPerfil: '#000',
+
+    backgroundGameOne: '#000',
+    primaryColorGameOne: '#000',
+    secondColorGameOne: '#000',
+
+    backgroundGameOne: '#000',
+    primaryColorGameOne: '#000',
+    secondColorGameOne: '#000',
+
+    backgroundGameTwo: '#000',
+    primaryColorGameTwo: '#000',
+    secondColorGameTwo: '#000',
+
+    backgroundGameThree: '#000',
+    primaryColorGameThree: '#000',
+    secondColorGameThree: '#000',
+
+    backgroundGameFour: '#000',
+    primaryColorGameFour: '#000',
+    secondColorGameFour: '#000',
+
+    backgroundMiniGames: '#000',
+    primaryColorMiniGames: '#000',
+    secondColorMiniGames: '#000',
+
+    backgroundMiniGame1: '#000',
+    primaryColorMiniGame1: '#000',
+    secondColorMiniGame1: '#000',
+
+    backgroundMiniGame2: '#000',
+    primaryColorMiniGame2: '#000',
+    secondColorMiniGame2: '#000',
+
+    backgroundMiniGame4: '#000',
+    primaryColorMiniGame4: '#000',
+    secondColorMiniGame4: '#000',
+
+    backgroundVariosMiniGames: '#000',
+    primaryColorVariosMiniGames: '#000',
+    secondColorVariosMiniGames: '#000',
+
+    backgroundVariosCreaFrase: '#000',
+    primaryColorCreaFrase: '#000',
+    secondColorCreaFrase: '#000',
+
+    backgroundVariosTodasFrase: '#000',
+    primaryColorTodasFrase: '#000',
+    secondColorTodasFrase: '#000',
+
+};
